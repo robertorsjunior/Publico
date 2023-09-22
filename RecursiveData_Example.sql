7a27baf42eb8b06a2fdf66a450df6c959f90ce13
Roberto Júnior <roberto_rsj@yahoo.com.br>
2023-09-22 10:43:18 -0300

    Fix: Adjust script code
    - Recursive query reevaluation

--- RecursiveData_Example.sql
+++ RecursiveData_Example.sql
@@ -1,21 +1,28 @@
 -- Drop table RecursiveData 
 -- Create Table RecursiveData (ID int, Description varchar(50), IDDependency int, ItemOrder int)
+-- Select * from RecursiveData
 
+delete RecursiveData; 
 Insert into RecursiveData (ID, Description, IdDependency, ItemOrder) Values (1, 'Dados 1', 0, 1);
 Insert into RecursiveData (ID, Description, IdDependency, ItemOrder) Values (2, 'Dados 2', 0, 2);
 Insert into RecursiveData (ID, Description, IdDependency, ItemOrder) Values (3, 'Dados 1.1', 1, 1);
 Insert into RecursiveData (ID, Description, IdDependency, ItemOrder) Values (4, 'Dados 1.2', 1, 2);
 Insert into RecursiveData (ID, Description, IdDependency, ItemOrder) Values (5, 'Dados 2.1', 2, 1);
+Insert into RecursiveData (ID, Description, IdDependency, ItemOrder) Values (6, 'Dados 1.2.1', 4, 1);
 
+with MyCTE
+as 
+(
 
-select Id, Description, Cast(ItemOrder as varchar(100)) as NIVEL
-from RecursiveData 
-where iddependency = 0
+    -- anchor
+    SELECT  ID, Description , IDDependency,1 AS [Level], CAST((ItemOrder) AS VARCHAR(MAX)) AS Hierarchy
+    FROM    RecursiveData rd 
+    WHERE   IDDependency = 0
 
-union ALL 
+	union all
 
-select Rd2.Id, rd2.Description, Cast(Rd1.ItemOrder as varchar(100)) + '->' + Cast(Rd2.ItemOrder as varchar(100))
-from RecursiveData Rd1
-inner join RecursiveData Rd2 on Rd1.Id = Rd2.IdDependency
-
-Order by 3
+    SELECT  t2.ID d, t2.Description, t2.IDDependency, M.[level] + 1 AS [Level], CAST((M.Hierarchy + '.' + CAST( t2.[ItemOrder] as varchar(max) )) AS VARCHAR(MAX)) AS Hierarchy
+    FROM    RecursiveData AS t2
+            JOIN MyCTE AS M ON t2.IDDependency = M.Id
+)
+SELECT * FROM MyCTE order by HIERARCHY
